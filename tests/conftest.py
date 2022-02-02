563122c590a69e097f93427219b1c9174f2ba330
viperior <viperior@gmail.com>
2022-02-02 15:38:14 -0800

    Remove placeholder code from template

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,1 @@
 """Pytest fixtures"""
-
-import pytest
-
-
-@pytest.fixture(scope="session")
-def fixture_foo():
-    """Return an object from fixture_foo"""
-    session_object = {
-        "foo": "bar",
-        "bar": "baz",
-    }
-
-    return session_object
