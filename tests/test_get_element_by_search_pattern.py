da5f2c740fed2208857f751e00fdae3bbe455cea
viperior <viperior@gmail.com>
2022-02-04 15:40:42 -0800

    Update test design to reflect the changes to the required parameter list for the get_element_by_search_pattern function (testing)

--- tests/test_get_element_by_search_pattern.py
+++ tests/test_get_element_by_search_pattern.py
@@ -1,5 +1,6 @@
 """Test the extract_save_data.get_element_by_search_pattern function"""
 
+import json
 import xml.etree.ElementTree
 
 import extract.extract_save_data
@@ -16,8 +17,13 @@ def test_get_element_by_search_pattern() -> None:
     """
     search_pattern = ".//world"
 
-    # TODO After refactor/rename of the function is complete, test this to ensure it works
     # TODO Change this test case to be parametrized and add additional search patterns as test input
-    element = extract.extract_save_data.get_element_by_search_pattern(search_pattern)
+    with open("config.json", "r", encoding="utf_8") as config_file:
+        config_data = json.load(config_file)
+
+    save_file_path = config_data["rimworld_save_file_path"]
+    tree = xml.etree.ElementTree.parse(save_file_path)
+    element = extract.extract_save_data.get_element_by_search_pattern(tree=tree,
+        element_search_pattern=search_pattern)
 
     assert isinstance(element, xml.etree.ElementTree.Element)
