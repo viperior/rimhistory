e3167bdd7e70a44933228bd475197b56b87a6833
viperior <viperior@gmail.com>
2022-02-04 15:26:08 -0800

    Add FIXME comment to get_element_by_search_pattern function regarding changing its parameters and return type (documentation)

--- extract/extract_save_data.py
+++ extract/extract_save_data.py
@@ -76,6 +76,7 @@ def get_element_by_search_pattern(element_search_pattern: str) -> xml.etree.Elem
         # TODO Include an optional parameter to limit the number of elements return
     logging.debug("-"*80) # FIXME Remove this unnecessary debug message
     logging.debug("Searching for element using pattern: %s", element_search_pattern)
+    # FIXME Modify this function to accept a tree rather than a path and remove this reference
     tree = xml.etree.ElementTree.parse("data/demosave truncated.rws")
     root = tree.getroot()
     element = root.find(element_search_pattern)
