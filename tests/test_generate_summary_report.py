464f3d16ab3d77fd3f3c86c1d3871443d04f1eeb
viperior <viperior@gmail.com>
2022-02-03 21:09:49 -0800

    Test the view.summary_report.generate_summary_report function by executing it without error (testing)

---
+++ tests/test_generate_summary_report.py
@@ -0,0 +1,17 @@
+"""Test the view.summary_report.generate_summary_report function"""
+
+import view.summary_report
+
+
+def test_generate_summary_report() -> None:
+    """Test the view.summary_report.generate_summary_report function
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # FIXME Build out the logic of this test case to finalize this contribution
+    # It should do more than generate the report. It should also test the generated report.
+    view.summary_report.generate_summary_report()
