026fb496b9d2522631123770e3125098ddcb8cb5
viperior <viperior@gmail.com>
2022-02-04 15:31:58 -0800

    Require an XML tree as a parameter in the get_element_by_search_pattern function

--- extract/extract_save_data.py
+++ extract/extract_save_data.py
@@ -63,10 +63,12 @@ def extract_rimworld_save_data() -> None:
     logging.debug("Recursion complete")
 
 
-def get_element_by_search_pattern(element_search_pattern: str) -> xml.etree.ElementTree.Element:
+def get_element_by_search_pattern(tree: xml.etree.ElementTree, element_search_pattern: str) ->\
+    xml.etree.ElementTree.Element:
     """Search for an XML element in a RimWorld save file using a search pattern
 
     Parameters:
+    tree (xml.etree.ElementTree): The XML tree to search
     element_search_pattern (str): Target element search pattern
 
     Returns:
@@ -76,8 +78,6 @@ def get_element_by_search_pattern(element_search_pattern: str) -> xml.etree.Elem
         # TODO Include an optional parameter to limit the number of elements return
     logging.debug("-"*80) # FIXME Remove this unnecessary debug message
     logging.debug("Searching for element using pattern: %s", element_search_pattern)
-    # FIXME Modify this function to accept a tree rather than a path and remove this reference
-    tree = xml.etree.ElementTree.parse("data/demosave truncated.rws")
     root = tree.getroot()
     element = root.find(element_search_pattern)
     logging.debug("Element information:\nTag: %s\nAttributes: %s\nText: %s\nKeys: %s",
