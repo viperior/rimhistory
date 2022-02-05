aec54f0d37cdb6df93da72f28d335d27f6a76ed8
viperior <viperior@gmail.com>
2022-02-04 22:37:18 -0800

    Create a pytest fixture, config_data, that yields the project configuration data as a dictionary, preventing excessive I/O when the test suite currently has no need to update the config data mid-run (testing)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,30 @@
 """Pytest fixtures"""
 
+import json
 import logging
 import os
 import shutil
 
 import pytest
+
+
+@pytest.fixture(scope="session")
+def config_data() -> dict:
+    """Return the project configuration data as a dictionary
+
+    Parameters:
+    None
+
+    Returns:
+    dict: Dictionary containing project configuration settings
+    """
+    config_file_name = "config.json"
+    logging.debug("Loading configuration data from: %s")
+
+    with open(config_file_name, "r", encoding="utf_8") as config_file:
+        project_config_data = json.load(config_file)
+
+    yield project_config_data
 
 
 @pytest.fixture(scope="session")
