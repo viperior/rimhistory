27554bab2d2226f4bf73a48009dc3d06d885e25f
viperior <viperior@gmail.com>
2022-02-04 20:34:04 -0800

    Test the generated HTML report's validity by checking for a DOCTYPE tag and checking the length of the HTML document (testing)

--- tests/test_generate_summary_report.py
+++ tests/test_generate_summary_report.py
@@ -1,5 +1,7 @@
 """Test the view.summary_report.generate_summary_report function"""
 
+import logging
+
 import view.summary_report
 
 
@@ -12,9 +14,31 @@ def test_generate_summary_report() -> None:
     Returns:
     None
     """
-    # FIXME Build out the logic of this test case to finalize this contribution
-    # It should do more than generate the report. It should also test the generated report.
+    # Parameters
     report_output_directory = "data/reports"
     report_file_name_base = "summary_report"
+
+    # Generate the report
     view.summary_report.generate_summary_report(output_directory=report_output_directory,
         output_file_name=report_file_name_base)
+    output_path = f"{report_output_directory}/{report_file_name_base}.html"
+
+    # Test the generated report
+    with open(output_path, "r", encoding="utf_8") as report_file:
+        doctype = report_file.readline()
+
+        # Check for a DOCTYPE tag on the first line
+        assert "DOCTYPE" in doctype
+        minimum_line_count_sentry = False
+
+        for line_counter, line in enumerate(report_file):
+            # Log the XML line data every 10th line at the DEBUG level
+            if line_counter % 10 == 0:
+                logging.debug(line)
+
+            # The report XML should have at least 20 lines of content
+            if line_counter >= 20:
+                minimum_line_count_sentry = True
+
+    # Check whether the generated file exceeds a minimum line length threshold
+    assert minimum_line_count_sentry is True
