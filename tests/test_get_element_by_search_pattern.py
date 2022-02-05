69c4fe1e014ba2aa1f947209bd85c8173eb76d34
viperior <viperior@gmail.com>
2022-02-04 22:38:20 -0800

    Use the config_data pytest fixture to retrieve the default save file path (testing)

--- tests/test_get_element_by_search_pattern.py
+++ tests/test_get_element_by_search_pattern.py
@@ -1,26 +1,21 @@
 """Test the XML element retrieval functions in extract_save_data"""
 
-import json
 import xml.etree.ElementTree
 
 import extract.extract_save_data
 
 
-def test_get_element_by_search_pattern() -> None:
+def test_get_element_by_search_pattern(config_data: dict) -> None:
     """Test the extract_save_data.get_element_by_search_pattern function
 
     Parameters:
-    None
+    config_data (dict): The project configuration data as a dictionary
 
     Returns:
     None
     """
     search_pattern = ".//world"
-
     # TODO Change this test case to be parametrized and add additional search patterns as test input
-    with open("config.json", "r", encoding="utf_8") as config_file:
-        config_data = json.load(config_file)
-
     save_file_path = config_data["rimworld_save_file_path"]
     tree = xml.etree.ElementTree.parse(save_file_path)
     element = extract.extract_save_data.get_element_by_search_pattern(tree=tree,
