8f28b87e01ad9ba0eff5b991aae0b47b997662ac
viperior <viperior@gmail.com>
2022-02-04 16:47:56 -0800

    Update the reference to the generate_summary_report function's parameters after modifying its design (testing)

--- tests/test_generate_summary_report.py
+++ tests/test_generate_summary_report.py
@@ -14,4 +14,7 @@ def test_generate_summary_report() -> None:
     """
     # FIXME Build out the logic of this test case to finalize this contribution
     # It should do more than generate the report. It should also test the generated report.
-    view.summary_report.generate_summary_report()
+    report_output_directory = "data/reports"
+    report_file_name_base = "summary_report"
+    view.summary_report.generate_summary_report(output_directory=report_output_directory,
+        output_file_name=report_file_name_base)
