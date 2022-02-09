3a47497516a4950699f8be630d38076d4a6ebc3f
viperior <viperior@gmail.com>
2022-02-09 01:02:22 -0800

    Update parameter list in test_get_pawn_count
    Use no parameters

--- tests/test_pawn_data.py
+++ tests/test_pawn_data.py
@@ -7,8 +7,7 @@ def test_get_pawn_count() -> None:
     """Test counting the number of pawns identified from the save data
 
     Parameters:
-    file_path (str): The path to the save game file
-    expected (int): The number of expected pawns to find in the corresponding save data
+    None
 
     Returns:
     None
