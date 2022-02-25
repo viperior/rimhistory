1c770ce2c6086aced0a2e735dbedad9000a1213d
viperior <viperior@gmail.com>
2022-02-24 17:40:32 -0800

    Test the SaveSeries class using an empty directory

--- tests/test_class_save_series.py
+++ tests/test_class_save_series.py
@@ -24,3 +24,24 @@ def test_class_save_series(test_data_directory: pathlib.Path, test_save_file_reg
                   expected_count, actual_count)
 
     assert actual_count == expected_count
+
+
+def test_empty_source_directory(tmp_path: pathlib.Path, test_save_file_regex: str) -> None:
+    """Test the SaveSeries class by attempting to load a series of saves from an empty directory
+
+    Parameters:
+    tmp_path (pathlib.Path): The path used to stage files needed for testing (fixture)
+    test_save_file_regex (str): The regex pattern matching the test input data file names (fixture)
+
+    Returns:
+    None
+    """
+    series = None
+
+    # Validate that the SaveSeries class throws an AssertionError when given an empty directory
+    try:
+        series = SaveSeries(save_dir_path=tmp_path, save_file_regex_pattern=test_save_file_regex)
+    except AssertionError as error:
+        assert isinstance(error, AssertionError)
+    finally:
+        assert series is None
